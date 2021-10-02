93a3521452861fc6347b868455cf9bc096c9d50e
eunbin <eunbines2@gmail.com>
2021-10-02 20:56:03 +0900

    Style: theme provider, global style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import Landing from './components/Landing';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  margin: 10px 100px;
-  border: 1px solid black;
-`;
+import { createGlobalStyle } from 'styled-components';
+import { Route, Switch } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './assets/styles/theme';
 
 function App() {
   return (
-    <Wrapper>
-      <Landing />
-    </Wrapper>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Container>
+          <Switch>
+            <Route exect path="/" component={Landing} />
+          </Switch>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #457f51;
+    font-family: "Courier New", monospace;
+  }
+
+  #root {
+    width: 90%;
+    height: 90%;
+    display:flex;
+  }
+`;
+
+const Container = styled.div`
+  background: #fefef7;
+  width: 100%;
+  border-radius: 20px;
+`;
 export default App;

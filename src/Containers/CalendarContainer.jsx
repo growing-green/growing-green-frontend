a769483568f1b9032526eb7a22e413dd1acde988
eunbin <eunbines2@gmail.com>
2021-10-11 03:16:34 +0900

    Chore: rename containers, update calendar icon style

--- src/Containers/CalendarContainer.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function CalendarContainer() {
-  return <h1>clarendar page</h1>;
-}
--- src/Containers/NewPlantContainer.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function NewPlantContainer() {
-  return <h1>new plant page</h1>;
-}

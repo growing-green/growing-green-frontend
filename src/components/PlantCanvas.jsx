43187b2393c3e70ee6d11212477679cb04affe0b
eunbin <eunbines2@gmail.com>
2021-10-14 05:15:24 +0900

    Add: if watering guage over the default guage then add penalty points, if penalty points over the 10 points then plant dead

--- src/components/PlantCanvas.jsx
+++ src/components/PlantCanvas.jsx
@@ -1,29 +1,60 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as PIXI from 'pixi.js';
 import styled from 'styled-components';
-import apiController from '../configs/apiController';
 
 import PlantContainer from '../pixi/displayObjects/PlantContainer';
 import Background from '../pixi/displayObjects/Background';
 import WateringContainer from '../pixi/displayObjects/WateringContainer';
 import GuageContainer from '../pixi/displayObjects/GuageContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
 
 import { imagePath } from '../pixi/pixiConstants';
-import { waterGuageValidator } from '../utils/waterGuageValidator';
-import { penaltyPointValidator } from '../utils/penaltyPointValidator';
+import { isWaterGuageOver } from '../utils/isWaterGuageOver';
+import { isPlantAlive } from '../utils/isPlantAlive';
+
+import { updatePlant } from '../redux/modules/plants';
+import { deletePlant } from '../redux/modules/plants';
 
 const loader = PIXI.Loader.shared;
 
 window.__PIXI_INSPECTOR_GLOBAL_HOOK__ &&
   window.__PIXI_INSPECTOR_GLOBAL_HOOK__.register({ PIXI: PIXI });
 
-export default function PlantCanvas({ plant }) {
+export default function PlantCanvas() {
+  const { allPlants } = useSelector((state) => state.plants);
+  const { plantId } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
   const canvas = useRef(null);
+  const [plant, setPlant] = useState(null);
 
   let app;
   let watering;
   let guage;
   let background;
+  let currentWaterGuage;
+  let defaultWaterGuage;
+  let currentPenaltyPoint;
+
+  useEffect(() => {
+    setPlant(allPlants[plantId]);
+
+    if (!plant) return;
+
+    (async (plant) => {
+      const myCanvas = canvas.current;
+      if (!canvas.current) return;
+
+      setup(myCanvas, plant);
+    })(plant);
+
+    return () => {
+      app?.ticker.remove(loop);
+      canvas.current = null;
+      app?.destroy();
+    };
+  }, [plant, plantId]);
 
   function loadImage() {
     return new Promise((resolve) => {
@@ -43,6 +74,7 @@ export default function PlantCanvas({ plant }) {
 
   async function setup(myCanvas, plantInfo) {
     if (!plantInfo) return;
+
     await loadImage();
 
     app = new PIXI.Application({
@@ -65,63 +97,80 @@ export default function PlantCanvas({ plant }) {
       species,
       sunGuage,
       waterGuage,
+      penaltyPoints,
     } = plantInfo;
 
-    background = new Background(app, isBlindUp, _id);
+    currentWaterGuage = waterGuage.currentGuage;
+    defaultWaterGuage = waterGuage.defaultGuage;
+    currentPenaltyPoint = penaltyPoints;
+
+    background = new Background(app, name, species, isBlindUp, _id);
     app.stage.addChild(background.container);
 
-    const plant = new PlantContainer(app, name, species, type, growthStage);
+    const plant = new PlantContainer(app, type, growthStage);
     app.stage.addChild(plant.container);
 
     watering = new WateringContainer(app);
     app.stage.addChild(watering.container);
-
     guage = new GuageContainer(app, sunGuage, waterGuage);
     app.stage.addChild(guage.container);
 
     app.ticker.add(loop);
   }
 
-  async function loop(e) {
+  function loop(e) {
     const totalGuageWidth = 400;
     const wateringPeriod = 5;
     const eachGuageWidth = totalGuageWidth / wateringPeriod;
 
     if (watering.wateringCan.isWatering === true) {
-      guage.waterGuage.width = guage.waterGuage.width + eachGuageWidth;
+      guage.waterGuage.width += eachGuageWidth;
       watering.wateringCan.isWatering = false;
-      await increaseWaterGuage(plant);
+
+      increaseWaterGuage(plant);
     }
   }
 
-  async function increaseWaterGuage(plant) {
-    const isValid = waterGuageValidator(plant);
+  function increaseWaterGuage(plant) {
+    const isOver = isWaterGuageOver(currentWaterGuage, defaultWaterGuage);
 
-    if (isValid === true) {
-      await apiController.put(`plants/${plant._id}`, {
-        state: 'water',
-        isIncrease: true,
-      });
+    if (isOver === true) {
+      currentWaterGuage += 1;
+
+      dispatch(
+        updatePlant({
+          plantId: plant._id,
+          data: {
+            state: 'water',
+            isIncrease: true,
+          },
+        }),
+      );
     } else {
-      const isAlive = penaltyPointValidator(plant);
+      const isAlive = isPlantAlive(currentPenaltyPoint);
 
       if (isAlive === true) {
-        await apiController.put(`plants/${plant._id}`, {
-          state: 'penalty',
-          isIncrease: true,
-        });
+        currentPenaltyPoint += 1;
+
+        alert(
+          `-1점 감점되었습니다. (현재 패널티 점수 ${currentPenaltyPoint}점)`,
+        );
+        dispatch(
+          updatePlant({
+            plantId: plant._id,
+            data: {
+              state: 'penalty',
+              isIncrease: true,
+            },
+          }),
+        );
       } else {
-        // await apiController.delete(`plants/${plant._id}`);
+        alert('식물이 죽었습니다.');
+
+        dispatch(deletePlant({ history, plantId: plant._id }));
       }
     }
   }
-
-  useEffect(() => {
-    (async (plant) => {
-      const myCanvas = canvas?.current;
-      setup(myCanvas, plant);
-    })(plant);
-  }, [plant]);
 
   return (
     <Wrapper>

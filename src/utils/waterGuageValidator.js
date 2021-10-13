7bb2a8d1007d34e26f91923896f82b49aa91e8a0
eunbin <eunbines2@gmail.com>
2021-10-14 05:16:09 +0900

    Chore: rename util functions for calcuate watering and penalty points

--- src/utils/waterGuageValidator.js
+++
@@ -1,9 +0,0 @@
-export function waterGuageValidator(plant) {
-  const { waterGuage } = plant;
-
-  if (waterGuage.defaultGuage < waterGuage.currentGuage + 1) {
-    return false;
-  }
-
-  return true;
-}

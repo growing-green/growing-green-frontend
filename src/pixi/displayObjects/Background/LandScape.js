f0510a468c29d9e83157201d7c4f2e0a60d29798
eunbin <eunbines2@gmail.com>
2021-10-14 23:20:58 +0900

    show different landscape according to time

--- src/pixi/displayObjects/Background/LandScape.js
+++ src/pixi/displayObjects/Background/LandScape.js
@@ -1,9 +1,12 @@
 import * as PIXI from 'pixi.js';
+import { isDay } from '../../../utils/isDay';
 const TextureCache = PIXI.utils.TextureCache;
 
 export default class Landscape extends PIXI.Sprite {
   constructor(x = 0, y = 0) {
-    const texture = TextureCache['dayTimeLandscape'];
+    const imageSource = isDay() ? 'dayTimeLandscape' : 'nightTimeLandscape';
+
+    const texture = TextureCache[imageSource];
     super(texture);
 
     this.anchor.set(0.5);

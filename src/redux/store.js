231090adf605b8c56481bea4612cea30c15c84e2
eunbin <eunbines2@gmail.com>
2021-10-16 16:28:31 +0900

    Deploy: remove logger middleware if production env

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,17 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
+let middleware;
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware = [thunk];
+} else {
+  middleware = [logger, thunk];
+}
+
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [logger, thunk],
+  middleware,
 });
 
 export default store;

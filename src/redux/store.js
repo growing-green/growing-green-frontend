8c651f4f69539b9d81a8c04a42ef212a55d266be
eunbin <eunbines2@gmail.com>
2021-11-06 00:02:09 +0900

    Fix: show redux logger middleware only in development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+
+let middleware = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middleware = [...middleware, logger];
+}
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [thunk],
+  middleware: middleware,
 });
 
 export default store;
